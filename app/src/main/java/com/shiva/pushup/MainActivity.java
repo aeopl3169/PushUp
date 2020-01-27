2612513aa9c19eb5f9a6647b82bca10b7e9d1b44
shashishiva9 <59079874+shashishiva9@users.noreply.github.com>
2020-01-27 07:00:32 +0530

    on

--- app/src/main/java/com/shiva/pushup/MainActivity.java
+++ app/src/main/java/com/shiva/pushup/MainActivity.java
@@ -34,12 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
-    String str = "ok";
-    String str2 = "";
-    int a = 10;
-    int branch1 = 12+13+14;
-    boolean t = true;
-    String str3 = "ok Axis";
+
 
     @Override
     protected void onStop() {
